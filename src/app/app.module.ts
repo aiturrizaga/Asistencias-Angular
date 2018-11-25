57c07178c82cf93abfd9cd5e7d466dc037bd2252
aiturrizaga <aiturrizaga@vallegrande.edu.pe>
2018-11-25 00:38:48 -0500

    Creando la vista de asistencias del alumno

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { TemplateComponent } from './pages/template/template.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    TemplateComponent
   ],
   imports: [
     BrowserModule,
